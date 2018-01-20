def340e4a78166f83b27f23042befa7fa9ef5ee4
3nan3 <nom3nan@gmail.com>
2018-01-20 17:02:33 +0900

    finised 11. adding enemy hp shot power and animations

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Rigidbody2Dコンポーネントを必須にする
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class Spaceship : MonoBehaviour
 {
 	// 移動スピード
@@ -21,6 +21,15 @@ public class Spaceship : MonoBehaviour
 	// 爆発のPrefab
 	public GameObject explosion;
 
+	// アニメーターコンポーネント
+	private Animator animator;
+
+	void Start ()
+	{
+		// アニメーターコンポーネントを取得
+		animator = GetComponent<Animator> ();
+	}
+
 	// 爆発の作成
 	public void Explosion ()
 	{
@@ -31,5 +40,11 @@ public class Spaceship : MonoBehaviour
 	public void Shot (Transform origin)
 	{
 		Instantiate (bullet, origin.position, origin.rotation);
+	}
+
+	// アニメーターコンポーネントの取得
+	public Animator GetAnimator()
+	{
+		return animator;
 	}
 }
